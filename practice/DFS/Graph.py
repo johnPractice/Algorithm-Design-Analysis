867546450865bb3775e86695eeadadaa991befaf
Reza Mousavian <rezamosavian1010@gmail.com>
2021-04-20 09:23:58 +0430

    edit bfs function

--- practice/DFS/Graph.py
+++ practice/DFS/Graph.py
@@ -3,6 +3,8 @@ from collections import defaultdict
 class Graph:
     def __init__(self):
         self.graph=defaultdict(list)
+        self.graph_vertex=defaultdict(list)
+        self.graph_level=defaultdict()
     def add_edge(self,u,v):
         if u in self.graph:
             self.graph[u].append(v)
@@ -24,31 +26,60 @@ class Graph:
         for v in list(self.graph):
             if v not in visited:
                 self._dfs_util(v,visited)
-
     def bfs(self,s):
         visited=[False] * (max(self.graph)+1)
+        l=0
         queue=[]
+        root_begin=s
+        self.graph_vertex[root_begin]=[]
         queue.append(s)
-        visited[s]=True
+        self.graph_level[s]={l:[s]}
         while queue:
             s=queue.pop(0)
-            print (s, end = " ")
-            for i in self.graph[s]:
-                if visited[i]==False:
+            # print(s)
+            self.graph_vertex[root_begin].append(s)
+            # print (s, end = " ")
+            # for i in range(len(self.graph[s])):
+            #     l+=1
+            #     if visited[i]==False:
+            #         queue.append(self.graph[s][i])
+            #         if l in  self.graph_level[s]:
+            #            self.graph_level[s][l].append(self.graph[s][i])
+            #         else:
+            #             self.graph_level[s].update({l:[]})
+            #             self.graph_level[s][l].append(self.graph[s][i])
+            #         visited[i]=True        
+            for i in (self.graph[s]):
+                # print(i)
+                if visited[l]==False:
                     queue.append(i)
-                    visited[i]=True
+                    visited[l]=True
+                l+=1
+        return self.graph_vertex[root_begin]
         
-
+    def print_path(self,s,v):
+        self.bfs(s)
+        # print(self.graph_vertex)
+        if v==s:
+            print(v)
+        elif v  in self.graph_vertex[s]:
+            i=self.graph_vertex[s].index(v)
+            return(self.graph_vertex[s][:i+1])
+            
+        else:
+            print('no path')
 
 
 def main():
     g=Graph()
-    g.add_edge(0,1)
-    g.add_edge(0,2)
-    g.add_edge(1,3)
+    g.add_edge(0,10)
+    g.add_edge(0,12)
+    g.add_edge(1,31)
     g.add_edge(3,1)
-    g.add_edge(10,11)
+    # g.add_edge(10,11)
+    # g.add_edge(10,12)
     # g.dfs()
-    g.bfs(0)
+    print(g.bfs(0))
+    # g.print_path(10,11)
 if __name__ == "__main__":
     main()
